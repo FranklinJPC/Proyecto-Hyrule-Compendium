6629c3b7fa2fa730ceadcd06f1bacd6c12a5dc86
Jhowellz <jhoelpatino26@hotmail.com>
2023-06-11 12:00:02 -0500

    API

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,32 +1,42 @@
 
-const verAPI = (req, res) => {
-    fetch('https://botw-compendium.herokuapp.com/api/v2/entry/49')
+// API de prueba
+const verAPI = (req,  res) => {
+    fetch('https://botw-compendium.herokuapp.com/api/v2/category/equipment')
       .then(response => {
         return response.json();
       })
       .then(data => {
         // Almacena el JSON en una variable
-        
-        const jsonVariable = data;
-        console.log(jsonVariable); 
-        res.json(data);
+        // const jsonVariable = data;
+        // data.data.forEach(element => {
+        //   console.log(element.name)
+        // });
+        // console.log(data.data[1])
+        //console.log(jsonVariable); 
+        res.render('armas', data
+          //data: `${data.data}`
+          // name: `${data.data}`,
+          // image: `${data.data}`,
+          // description: `${data.data}`
+        )
+        //res.json(data);
       })
       .catch(error => {
         console.log(error);
         res.status(500).json({ error: 'Error al obtener los datos de la API' });
       });
-  };
+};
+
+
+// Envio a las rutas
 
 const renderIndex = (req,res)=>{
     res.render('index')
 }
-// const renderAPI = (req, res) =>{
-//     res.json(controllerAPI)
-// }
 
 module.exports ={
     renderIndex,
-    verAPI
+    verAPI,
     //....
     // Se colocan las demas rutas 
     // que se vayan creado
--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,10 +1,12 @@
 const {Router} = require('express')
-const {/*rutas*/renderIndex,  verAPI} = require('../controllers/index.controllers.js')
+const {/*rutas*/renderIndex,  verAPI, renderArmas} = require('../controllers/index.controllers.js')
+
 
 const router = Router()
 
 router.get('/', renderIndex)
 router.get('/apiPrueba', verAPI)
+router.get('/armas', verAPI)
 //....
 // Se colocan las demas 
 
